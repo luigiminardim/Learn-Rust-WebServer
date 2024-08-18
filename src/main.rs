4bb36a2c28fa0b6dafe347856d849efa37af5b57
Luigi Minardi <luigiminardim@gmail.com>
2024-08-17 22:10:50 -0300

    feat: :sparkles: single threaded server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::{
+    fs,
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.0:3000").unwrap();
+    println!("Server listening on port 3000");
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        print!("Connection established!");
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    stream.write_all(response.as_bytes()).unwrap();
 }
