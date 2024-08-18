6e8c70bf5a727ddd913c93f1888785ac1317bfce
luigiminardim <luigiminardim@gmail.com>
2024-08-18 19:10:19 -0300

    feat: :sparkles: multi threaded server

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,35 @@ use std::{
     fs,
     io::{BufRead, BufReader, Write},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
 
+use web_server::ThreadPool;
+
 fn main() {
-    let listener = TcpListener::bind("127.0.0.0:3000").unwrap();
+    let pool = ThreadPool::new(4);
+    let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
     println!("Server listening on port 3000");
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
-        print!("Connection established!");
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    println!("{request_line}");
+    let (status_line, filename) = match request_line.as_str() {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
     let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
